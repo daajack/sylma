a1e31b8c42c5b7f10a85d83d98dc20ec6886c1d5
rodolphe.gerber <rodolphe.gerber@d4344482-c171-11dd-9d7c-c7da60823653>
2008-12-03 23:32:16 +0000

    Changements dans l'arborescence du dossier /lib Modifier index.php
    include(../Window.php) => include(Window.php)
    
    Ajouter :
    
    Controler::setArgument('title', 'OnSite');
    
    db::setArgument('host', 'localhost');
    db::setArgument('user', '***');
    db::setArgument('password', '***');

---
+++ trunk/lib/Sylma.php
@@ -0,0 +1,16 @@
+<?php
+
+require('Error.php');
+require('Global.php');
+
+require('HTML.php');
+require('HTML_Form.php');
+require('Field.php');
+require('Divers.php');
+require('Controler.php');
+require('Action.php');
+require('User.php');
+require('Database.php');
+require('spyc/spyc.php');
+
+require('Window.php');

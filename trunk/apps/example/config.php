08a020b8a380e2841a81cc517baa3d6716f2ca73
rodolphe.gerber <rodolphe.gerber@d4344482-c171-11dd-9d7c-c7da60823653>
2008-12-04 16:21:40 +0000

    Modification des chemins d'accès. c.f. index.php et config.php dans apps/example

---
+++ trunk/apps/example/config.php
@@ -0,0 +1,11 @@
+<?php
+  
+define('PATH_SYLMA', '../sylma/lib');
+define('PATH_PHP', 'protected');
+define('SITE_TITLE', 'Lemon.web');
+
+$aDB = array(
+  'host' => 'localhost',
+  'user' => 'root',
+  'password' => '',
+);

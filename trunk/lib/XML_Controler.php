665063da2b63d81b61049e9c32f562e813505932
rodolphe.gerber <rodolphe.gerber@d4344482-c171-11dd-9d7c-c7da60823653>
2009-05-13 17:20:18 +0000

    General : test statut before sending messages for best speed Action : returns ok, added Action_Array class, le:php array, ... XML : - Element : added isText(), isElement(), test(), testAttribute(), less report message - Text : added isText(), isElement()

--- trunk/lib/XML_Controler.php
+++ trunk/lib/XML_Controler.php
@@ -1,52 +1,56 @@
 <?php
+
+class XML_Controler {
   
-  class XML_Controler {
+  private static $oMessages = null;
+  private static $aStats = array();
+  
+  public static function init() {
     
-    private static $oMessages = null;
-    private static $aStats = array();
-    
-    public static function init() {
-      
-      self::$oMessages = new Messages(array('error', 'warning'));
-    }
-    
-    public static function viewStats() {
-      
-      // self::addStat('load', 1); // precog ;)
-      
-      $oResult = new XML_Document('statistics');
-      
-      $oResult->addArray(self::$aStats, 'category');
-      return $oResult->parseXSL(new XML_Document('/users/controler/stats.xsl'));
-    }
-    
-    public static function addStat($sKey, $iWeight = 1) {
-      
-      if (!array_key_exists($sKey, self::$aStats)) self::$aStats[$sKey] = $iWeight;
-      else self::$aStats[$sKey] += $iWeight;
-    }
-    
-    public static function getMessages() {
-      
-      return self::$oMessages;
-    }
-    
-    public static function addMessage($mValue, $sStatut = 'notice', $aArguments = array()) {
-      
-      // if (Controler::isAdmin()) {
-        
-        $aMessage = array(
-          new HTML_Strong('XML', array('style' => 'text-decoration: underline;')),
-          ' : ',
-          $mValue);
-        
-        // $aMessage = array_merge($aMessage, array(new HTML_Br, Controler::getBacktrace()));
-        if ($sStatut == 'error') $aMessage = array_merge($aMessage, array(new HTML_Br, Controler::getBacktrace()));
-        
-        // if (Controler::isReady()) Controler::addMessage($aMessage, $sStatut, $aArguments);
-        self::getMessages()->addMessage(new Message($aMessage, $sStatut, $aArguments));
-        // echo new HTML_Tag('pre', $aMessage, array('class' => 'message-'.$sStatut));
-      // }
-    }
+    self::$oMessages = new Messages(array('error', 'warning', '_report', 'notice'));
   }
   
+  public static function viewStats() {
+    
+    // self::addStat('load', 1); // precog ;)
+    
+    $oResult = new XML_Document('statistics');
+    
+    $oResult->addArray(self::$aStats, 'category');
+    return $oResult->parseXSL(new XML_Document('/users/controler/stats.xsl'));
+  }
+  
+  public static function addStat($sKey, $iWeight = 1) {
+    
+    if (!array_key_exists($sKey, self::$aStats)) self::$aStats[$sKey] = $iWeight;
+    else self::$aStats[$sKey] += $iWeight;
+  }
+  
+  public static function useStatut($sStatut) {
+    
+    return self::getMessages()->useStatut($sStatut);
+  }
+  
+  public static function getMessages() {
+    
+    return self::$oMessages;
+  }
+  
+  public static function addMessage($mValue, $sStatut = 'notice', $aArguments = array()) {
+    
+    // if (Controler::isAdmin()) {
+      
+      $aMessage = array(
+        new HTML_Strong('XML', array('style' => 'text-decoration: underline;')),
+        ' : ',
+        $mValue);
+      
+      // $aMessage = array_merge($aMessage, array(new HTML_Br, Controler::getBacktrace()));
+      if ($sStatut == 'error') $aMessage = array_merge($aMessage, array(new HTML_Br, Controler::getBacktrace()));
+      
+      // if (Controler::isReady()) Controler::addMessage($aMessage, $sStatut, $aArguments);
+      self::getMessages()->addMessage(new Message($aMessage, $sStatut, $aArguments));
+      // echo new HTML_Tag('pre', $aMessage, array('class' => 'message-'.$sStatut));
+    // }
+  }
+}

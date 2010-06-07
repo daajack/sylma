f9d9041e90d1ee96b3da033cfdb95b24c86a7cc3
rodolphe.gerber <rodolphe.gerber@d4344482-c171-11dd-9d7c-c7da60823653>
2010-06-07 09:51:23 +0000

    Debug in form send values method Added Window debug functions

--- processors/form/FormProcessor.php
+++ processors/form/FormProcessor.php
@@ -3,12 +3,19 @@
 class FormProcessor extends XML_Processor  {
   
   private $oForm;
+  private $bOptionsKey = false;
   
   public function onElement($oElement, XML_Action $oAction) {
     
     switch ($oElement->getName()) {
       
       case 'form' :
+        
+        if ($sOptionsKey = $oElement->getAttribute('options-key')) {
+          
+          $this->bOptionsKey = strtobool($sOptionsKey);
+          $oElement->setAttribute('options-key');
+        }
         
         $oForm = $this->oForm = new HTML_Form();
         $oForm->cloneAllAttributes($oElement);
@@ -19,10 +26,44 @@ class FormProcessor extends XML_Processor  {
         
       break;
       
+      case 'list-fields' :
+        
+        $oResult = null;
+        
+        if (!$oForm = $this->getForm()) {
+          
+          dspm(array(t('Aucun formulaire n\'a été instancié !'), $oElement->messageParse()), 'action/error');
+          
+        } else {
+          
+          if (!$sFields = $oElement->getValue()) dspm(xt('Aucune valeur dans l\'élément %s du formulaire', $oElement));
+          else {
+            
+            $oResult = new XML_Document('root');
+            $aField = explode(',', $sFields);
+            
+            foreach ($aField as &$sField) {
+              
+              $oResult->add($oForm->buildField(new XML_Element('field', null, array('id' => trim($sField))), $this->bOptionsKey));
+            }
+          }
+        }
+        
+        return $oResult;
+        
+      break;
+      
+      case 'all-fields' :
+        
+        if (!$oForm = $this->getForm()) dspm(array(t('Aucun formulaire n\'a été instancié !'), $oElement->messageParse()), 'action/error');
+        else return $oForm->buildAllFields($this->bOptionsKey);
+        
+      break;
+      
       case 'field' :
         
         if (!$oForm = $this->getForm()) dspm(array(t('Aucun formulaire n\'a été instancié !'), $oElement->messageParse()), 'action/error');
-        else return $oForm->buildField($oElement);
+        else return $oForm->buildField($oElement, $this->bOptionsKey);
         
       break;
     }

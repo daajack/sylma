d7bb2ad6b5ca784527f623bee17ac5aba1939469
rodolphe.gerber <rodolphe.gerber@d4344482-c171-11dd-9d7c-c7da60823653>
2010-09-24 07:42:40 +0000

    + le:namespace XMLSchema
    Module :
    + get/setNamespace()
    + get/setPrefix()

--- trunk/lib/Module.php
+++ trunk/lib/Module.php
@@ -3,6 +3,8 @@
 class Module {
   
   private $oDirectory = null;
+  private $sNamespace = SYLMA_NS_XHTML;
+  private $sPrefix = '';
   
   public function setDirectory($sPath) {
     
@@ -12,6 +14,26 @@ class Module {
   public function getDirectory() {
     
     return $this->oDirectory;
+  }
+  
+  public function setNamespace($sNamespace) {
+    
+    $this->sNamespace = $sNamespace;
+  }
+  
+  public function getNamespace() {
+    
+    return $this->sNamespace;
+  }
+  
+  public function setPrefix($sPrefix) {
+    
+    $this->sPrefix = $sPrefix;
+  }
+  
+  public function getPrefix() {
+    
+    return $this->sPrefix;
   }
   
   public function getDocument($sPath) {

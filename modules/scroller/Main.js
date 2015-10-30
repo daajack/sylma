7257705793d4d38950a2433c68cf0181c42f2bcb
Rodolphe Gerber <daajack@lemon.ch>
2015-10-30 16:00:29 +0100

    + modules.scroller.Main.checkPosition()

--- modules/scroller/Main.js
+++ modules/scroller/Main.js
@@ -6,17 +6,80 @@ sylma.modules.scroller.Main = new Class({
 
   Extends : sylma.html.Main,
 
+  needUpdate : true,
+
   onLoad : function() {
 
-    this.loadLinks(this.getMenuItems());
+    var delay = 200;
+
+    var menus = $$(this.options.menus);
+    var links = $$(this.options.links);
+    var anchors = $$(this.options.anchors);
+
+    this.anchors = this.loadAnchors(anchors);
+    this.menus = this.loadMenus(menus, this.anchors);
+
+    this.loadLinks(links.append(menus));
+
+    var callback = function() {
+
+      this.checkPosition();
+      this.needUpdate = true;
+
+    }.bind(this);
+
+    window.addEvent('scroll', function() {
+
+      if (this.needUpdate) {
+
+        window.setTimeout(callback, delay);
+        this.needUpdate = false;
+      }
+
+    }.bind(this));
   },
 
-  getMenuItems : function() {
+  loadAnchors: function (nodes) {
 
-    return $$(this.get('menu'));
+    var anchors = {};
+
+    nodes.each(function(anchor) {
+
+      var name = anchor.getAttribute('name');
+
+      anchors[name] = {
+        node : anchor,
+        //links : [],
+        menu : null
+      };
+    });
+
+    return anchors;
   },
 
-  loadLinks : function(links) {
+  loadMenus: function (nodes, anchors) {
+
+    var menus = {};
+
+    nodes.each(function(node) {
+
+      var name = node.get('href').substr(1);
+      var menu = {
+        node : node
+      };
+
+      menus[name] = menu;
+
+      if (anchors[name]) {
+
+        anchors[name].menu = menu;
+      }
+    });
+
+    return menus;
+  },
+
+  loadLinks : function(links, anchors) {
 
     var main = this;
 
@@ -25,36 +88,71 @@ sylma.modules.scroller.Main = new Class({
         wheelStops: false
     });
 
+    links.each(function(link) {
+
+      var name = link.getAttribute('href').substr(1);
+
+      link.store('name', name);
+      //anchors[name].links.push(link);
+    });
+
+    var menus = this.menus;
+
     links.addEvent('click', function() {
 
-      main.activateMenu(this);
+      main.activateMenu(menus[this.retrieve('name')]);
     });
+
+  },
+
+  loadCurrent : function() {
 
     var hash = window.location.hash;
 
     if (hash) {
 
-      var current = $$('a[href=' + hash + ']');
+      var current = this.menus[hash];
 
-      if (current.length) {
+      if (current) {
 
-        current[0].click();
+        current.node.click();
       }
-
     }
   },
 
-  activateMenu : function(el) {
+  checkPosition : function() {
 
-    this.getMenuItems().each(function(link) {
+    var offset = Infinity;
+    var windowY = window.getScroll().y;
+    var current;
 
-      link.getParent().removeClass('active');
+    Object.each(this.anchors, function(anchor) {
+
+      var currentOffset = Math.abs(anchor.node.getPosition().y - windowY);
+
+      if (currentOffset < offset) {
+
+        current = anchor;
+        offset = currentOffset;
+      }
     });
 
-    if (el) {
+    if (current) {
 
-      var item = el.getParent();
-      item.addClass('active');
+      this.activateMenu(current.menu);
+    }
+  },
+
+  activateMenu : function(menu) {
+
+    Object.each(this.menus, function(menu) {
+
+      menu.node.getParent().removeClass('active');
+    });
+
+    if (menu) {
+
+      menu.node.getParent().addClass('active');
     }
   }
 });
